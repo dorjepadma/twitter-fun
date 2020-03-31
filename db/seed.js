7792454e63932d35c00362dc01db58ec43b273eb
dorjepadma <dorjepadma@gmail.com>
2020-03-30 18:02:15 -0700

    still not there

---
+++ db/seed.js
@@ -0,0 +1,19 @@
+const Tweet = require('../lib/models/Tweets');
+const Comment = require('../lib/models/Comments');
+const chance = require('chance').Chance();
+
+
+module.exports = async({ tweetsToCreate = 16, commentsToCreate = 28 } = {}) => {
+  
+  const handles = ['@happyvixen', '@mellowdude', '@spunkypuppy'];
+  const tweets = await Tweet.create([...Array(tweetsToCreate)].map(() => ({
+    handle: chance.pickone(handles),
+    text: chance.sentence()
+  })));
+
+  await Comment.create([...Array(commentsToCreate)].map(() => ({
+    tweetId: chance.pickone(tweets)._id,
+    handle: chance.pickone(handles),
+    text: chance.sentence()
+  })));
+};
